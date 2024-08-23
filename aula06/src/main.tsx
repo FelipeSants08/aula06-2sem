a3d4bbc10dc59c17e6104b8b2cd1994792c2804a
JulioSamuelOliveira <juliosamuoliveira@gmail.com>
2024-08-23 09:18:26 -0300

    Pacote rounter-dom instalado e componentes de otas criados e importados no main.tsx.

--- aula06/src/main.tsx
+++ aula06/src/main.tsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Home from './routes/Home/index.tsx'
+import Produtos from './routes/Produos/index.tsx'
+import EditarProdutos from './routes/EditarProdutos/index.tsx'
+import Error from './routes/Error/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
