9a4e8f6735367eee770867e4d577e32c87083d5f
luis-hsl <luis_henrique04c@hotmail.com>
2024-08-23 09:40:13 -0300

    Arquivo de transição OUTLET criado

--- aula06/src/routes/Error/index.tsx
+++ aula06/src/routes/Error/index.tsx
@@ -1,7 +1,7 @@
 export default function Error() {
     return (
         <div>
-            <h1>Componente Erro</h1>
+            <h1>Error 404 - Página não encntrada...</h1>
         </div>
     );
 }
