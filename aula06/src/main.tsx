c3652c79197d7d0fd6efbec9c65876edfcc7bae0
DGMMX <dgmmxs2@otlook.com>
2024-08-23 09:35:07 -0300

    é isso

---
+++ aula06/src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+import { createBrowserRouter } from 'react-router-dom'
+import { default as EditarProdutos, default as Home } from './routes/EditarProdutos/index.tsx'
+import Error from './routes/Error/index.tsx'
+import Produtos from './routes/Produtos/index.tsx'
+
+const routes = createBrowserRouter([
+  {
+    path: "/", element: <App />, errorElement: <Error />, children: [
+      { path: "/", element: <Home /> },
+      { path: "/produtos", element: <Produtos /> },
+      { path: "/editar/produtos/:id", element: <EditarProdutos /> },
+    ]
+  }]);
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
