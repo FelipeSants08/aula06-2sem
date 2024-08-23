c2c4f0bdbb935028039f1765b2b01000b7292476
Samir Neto <samihneto@gmail.com>
2024-08-23 09:40:15 -0300

    Arquivo de transição OUTLET criado.

---
+++ aula06/src/main.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.tsx'
+import Produtos from './routes/Produtos/index.tsx'
+import EditarProdutos from './routes/EditarProdutos/index.tsx'
+import Error from './routes/Error/index.tsx'
+
+const routes = createBrowserRouter([
+  {
+    path: "/", element:<App/>, errorElement:<Error/>, children:[
+      {path:"/", element:<Home/>},
+      {path:"/produtos", element:<Produtos/>},
+      {path:"/editar/produtos/:id", element:<EditarProdutos/>},
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={routes}/>
+  </StrictMode>,
+)
