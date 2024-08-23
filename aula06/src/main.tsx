5fb79b30ffcafa6b6d93845974fc20e032b8c700
MarceloScoleso <marce.scolesojr@gmail.com>
2024-08-23 09:30:53 -0300

    rotas criadas

--- aula06/src/main.tsx
+++ aula06/src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import EditarProdutos from './routes/EditarProdutos/index.tsx'
+import Error from './routes/Error/index.tsx'
+import Home from './routes/Home/index.tsx'
+import Produtos from './routes/Produtos/index.tsx'
+
+const routes = createBrowserRouter([{
+  path: "/", element: <App />, errorElement: <Error />, children: [
+    { path: "/", element: <Home /> },
+    { path: "/", element: <Produtos /> },
+    { path: "/", element: <EditarProdutos /> },
+  ]
+}]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
