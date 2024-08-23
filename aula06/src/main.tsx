ba455263012abb4d2ee3796569eaf114149633fb
mateuscastronappe <teteucastro31@gmail.com>
2024-08-23 09:19:05 -0300

    Pacote router dom instalado e componentes de rotas criados e importados no main.tsx

--- aula06/src/main.tsx
+++ aula06/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+createBrowserRouter
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
