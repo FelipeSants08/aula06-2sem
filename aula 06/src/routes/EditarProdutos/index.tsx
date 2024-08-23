ae4d715c9dcf4da9dc3f112f8836f5aa840a0319
Caepena <caepenafiel@gmail.com>
2024-08-23 09:30:51 -0300

    Estrutura de rotas criadas no main.tsx

---
+++ aula 06/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,5 @@
+export default function EditarProdutos() {
+    return (
+        <h1>Componente EditarProdutos</h1>
+    )
+}
---
+++ aula 06/src/routes/Error/index.tsx
@@ -0,0 +1,5 @@
+export default function Error() {
+    return (
+        <h1>Componente Error</h1>
+    )
+}
